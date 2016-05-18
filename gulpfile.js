4220edd5b2a425d5b13a90f373628a5ee2c7b037
jackrugile <jackrugile@gmail.com>
2016-05-17 23:53:42 -0600

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+/*==============================================================================
+Gulp
+==============================================================================*/
+
+var gulp = require( 'gulp' ),
+	gulpLoadPlugins = require( 'gulp-load-plugins' ),
+	p = gulpLoadPlugins();
+
+function handleError( err ) {
+	console.log( err.toString() );
+	this.emit( 'end' );
+}
+
+/*==============================================================================
+Clean
+==============================================================================*/
+
+gulp.task( 'clean', function() {
+	gulp.src( [ 'dist/index.html', 'dist/css' ], { 
+			read: false
+		})
+		.pipe( p.rimraf() )
+		.pipe( p.notify( 'Gulp Clean Task Complete' ) );
+});
+
+/*==============================================================================
+HTML
+==============================================================================*/
+
+gulp.task('html', function() {
+	gulp.src( 'src/index.html' )
+	.pipe( gulp.dest( 'dist' ) )
+	.pipe( p.notify( 'Gulp HTML Task Completed' ) );
+});
+
+/*==============================================================================
+Styles
+==============================================================================*/
+
+gulp.task( 'styles', function() {
+	return gulp.src( 'src/scss/main.scss' )
+		.pipe( p.sass( {
+			style: 'expanded'
+		}))
+		.on( 'error', p.sass.logError)
+		.pipe( p.rename( 'main.css' ) )
+		.pipe( gulp.dest( 'dist/css' ) )
+		.pipe( p.autoprefixer() )
+		.pipe( p.minifyCss( { advanced: false } ) )
+		.pipe( p.rename( 'main.min.css' ) )
+		.pipe( gulp.dest( 'dist/css' ) )
+		.pipe( p.notify( 'Gulp Styles Task Completed' ) );
+});
+
+/*==============================================================================
+Watch
+==============================================================================*/
+
+gulp.task('watch', function() {
+	gulp.watch( 'src/index.html', [ 'html' ] );
+	gulp.watch( 'src/scss/**/*', [ 'styles' ] );
+});
+
+/*==============================================================================
+Default
+==============================================================================*/
+
+gulp.task( 'default', [ 'html', 'styles' ], function() {
+	gulp.start( 'watch' );
+});
